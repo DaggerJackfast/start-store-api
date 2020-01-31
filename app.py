d6805f054defa7918ad5122fc4ccaa5d2eb316f8
Aibek <aibek.abdykasymov@gmail.com>
2020-02-01 00:36:53 +0600

    Added checkout order

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_cors import CORS
-from config import config
+from config import config, basedir
 
 migrate = Migrate()
 admin = Admin(name='start-store', template_mode="bootstrap3")
@@ -24,12 +24,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 admin.init_app(app)
 
 products = []
-with open('./fixtures/products.json') as p:
+with open(os.path.join(basedir, 'fixtures/products.json')) as p:
     products = [dict(x, **{'id': i}) for i, x in enumerate(json.loads(p.read()))]
 
 shipping = []
-with open('./fixtures/shipping.json') as p:
+with open(os.path.join(basedir, './fixtures/shipping.json')) as p:
     shipping = [dict(x, **{'id': i}) for i, x in enumerate(json.loads(p.read()))]
+
+orders = []
 
 
 @app.route('/')
@@ -50,6 +52,20 @@ def get_product(product_id):
 @app.route('/shipping')
 def get_shipping():
     return jsonify(shipping)
+
+
+@app.route('/order/checkout', methods=['POST'])
+def order_checkout():
+    data = request.get_json()
+    print('data: ', data)
+    orders.append(data)
+    print('orders: ', orders)
+    return jsonify({'status': 'success', 'message': 'The order successfully checked.'})
+
+
+@app.route('/orders')
+def get_orders():
+    return jsonify(orders)
 
 
 if __name__ == '__main__':
