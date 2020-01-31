24433294daf1b793caa4c8c96fc7f5a0bfcefc69
Aibek <aibek.abdykasymov@gmail.com>
2020-02-01 02:48:20 +0600

    Added order models

--- models.py
+++ models.py
@@ -1,17 +1,34 @@
 from .app import db
 
 
-class Product(db.Model):
+class Updatable(db.Model):
+    __abstract__ = True
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), server_default=db.func.now(), server_onupdate=db.func.now())
+
+
+class Product(Updatable):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     description = db.Column(db.String(50))
     price = db.Column(db.Numeric(precision=8, asdecimal=False, decimal_return_scale=None))
     enabled = db.Column(db.Boolean())
-    created_at = db.Column(db.DateTime(), server_default=db.func.now())
-    updated_at = db.Column(db.DateTime(), server_default=db.func.now(), server_onupdate=db.func.now())
 
 
 class Shipping(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     price = db.Column(db.Numeric(precision=8, asdecimal=False, decimal_return_scale=None))
+
+
+class Order(Updatable):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    address = db.Column(db.String(), nullable=False, unique=True)
+    products = db.relationship('Product', secondary='OrderItems', lazy='subquery')
+
+
+class OrderItems(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False,),
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
